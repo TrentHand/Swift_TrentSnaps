4dcf0ca14b22f1dffd71b4ee7deeccac64996efa
Trent Hand <livefree1983@gmail.com>
2017-03-22 10:42:53 -0500

    Images are saving to FireBase
    This images are still very large.  Next step is to make the images much smaller

--- TrentSnaps/PictureViewController.swift
+++ TrentSnaps/PictureViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
 class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var descriptionTextField: UITextField!
     @IBOutlet weak var nextButton: UIButton!
@@ -20,7 +22,7 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         imagePicker.delegate = self
     }
@@ -37,7 +39,7 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
         
         imagePicker.dismiss(animated: true, completion: nil)
     }
-
+    
     @IBAction func cameraTapped(_ sender: Any) {
         
         //IN PRODUCTION, THIS NEEDS TO BE MOVED TO "CAMERA" INSTEAD OF SAVEDPHOTOSALBUM
@@ -48,5 +50,30 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
         present(imagePicker, animated: true, completion: nil)
     }
     @IBAction func nextTapped(_ sender: Any) {
+        
+        nextButton.isEnabled = false
+        //this is how we store things to Firebase.  Make sure to import Firebase & FirebaseStorage
+        //.child tells us to move into a folder in FB.  We've told it to look for a folder named "images"
+        let imagesFolder = FIRStorage.storage().reference().child("images")
+        
+        let imageData = UIImagePNGRepresentation(imageView.image!)
+        
+        imagesFolder.child("images.png").put(imageData!, metadata: nil, completion: {(metadata, error) in
+            print("We tried to upload")
+            if error != nil {
+                print("We have an error \(error)")
+            } else {
+                
+                //moves them to the selectUser view
+                self.performSegue(withIdentifier: "selectUserSegue", sender: nil)
+            }
+        })
+        
+        
+    }
+    
+    //this is what happens before the segue is completed
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
     }
 }

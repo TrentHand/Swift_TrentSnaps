dc4eb64dabd0deb548cb154742866e91d1ab0c0f
Trent Hand <livefree1983@gmail.com>
2017-03-22 12:59:58 -0500

    users print on the tableView
    We can now select a user

--- TrentSnaps/SelectUserViewController.swift
+++ TrentSnaps/SelectUserViewController.swift
@@ -7,29 +7,55 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
-class SelectUserViewController: UIViewController {
+class SelectUserViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    //the array of users we get back from Firebase
+    var users : [User] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        //pull users from Firebase
+        FIRDatabase.database().reference().child("users").observe(FIRDataEventType.childAdded, with: {(snapshot) in
+            print(snapshot)
+            
+            let user = User()
+            user.email = (snapshot.value as! NSDictionary)["email"] as! String
+            user.uid = snapshot.key
+            //adds the new user to the users in FB
+            self.users.append(user)
+            //reloads the tableView to show the new users
+            self.tableView.reloadData()
+        })
+
+        
+
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //setting the number of rows to the number of users
+        return users.count
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        //set the user to the one selected from the cell
+        let user = users[indexPath.row]
+        
+        cell.textLabel?.text = user.email
+        return cell
     }
-    */
 
 }
